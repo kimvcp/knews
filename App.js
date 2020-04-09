ebbe19ea41cf2677c6597d30f5dd4288590a1422
Vichaphol Thamsuthikul <vichaphol.t@ku.th>
2020-04-09 22:55:07 +0700

    init react native

---
+++ App.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+});
